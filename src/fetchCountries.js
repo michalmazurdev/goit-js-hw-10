f61a47a109ca0aac41b8af2c612f87cb7e01f858
michalmazurdev <m.mazur40@gmail.com>
2023-04-29 04:03:03 +0200

    task finished

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,55 +1,20 @@
+const API_URL = 'https://restcountries.com/v3.1/name/';
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+  return fetch(
+    `${API_URL}${name}?fields=name,capital,population,flags,population,languages`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
   });
 }
-const inputEl = document.getElementById('search-box');
-const listEl = document.querySelector('.country-list');
-const countryInfoEl = document.querySelector('.country-info');
-import debounce from 'lodash.debounce';
-import throttle from 'lodash.throttle';
-import { Notify } from 'notiflix';
-function renderCountries(data) {
-  countryInfoEl.innerHTML = '';
-  listEl.innerHTML = '';
-  if (data.length === 1) {
-    console.log('1');
-    let country = data[0];
-    let languages = Object.values(country.languages).join(', ');
-    countryInfoEl.innerHTML = `
-    <h2><img height=36 src=${country.flags.svg} alt='Flag of ${country.name.official}'>${country.name.official}</h2>
-    <ul>
-      <li><b>Capital:</b> ${country.capital}</li>
-      <li><b>Population:</b> ${country.population}</li>
-      <li><b>Languages:</b> ${languages}</li>
-    </ul>
-    `;
-  } else if (data.length > 1 && data.length < 11) {
-    console.log('2-10');
-    let markup = ``;
-    for (let country of data) {
-      markup += `<li><img height=19px src=${country.flags.svg} alt='Flag of ${country.name.official}'>${country.name.official}</li>`;
-      listEl.innerHTML = markup;
-    }
-  } else {
-    console.log('too many');
-    Notify.info('Too many matches found. Please enter a more spcific name.');
-  }
-}
-
-inputEl.addEventListener('input', event => {
-  console.log(sevent.target.value);
-});
-
-fetchCountries('poland')
-  .then(data => {
-    console.log(data);
-    renderCountries(data);
-  })
-  .catch(error => {
-    console.log(error, 'oups, something went wrong');
-    Notify.failure('Oops, there is no country with that name');
-  });
+// fetchCountries('s')
+//   .then(data => {
+//     console.log(data);
+//     renderCountries(data);
+//   })
+//   .catch(error => {
+//     console.log(error, 'oups, something went wrong');
+//     Notify.failure('Oops, there is no country with that name');
+//   });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.getElementById('search-box');
+const listEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+function renderCountries(data) {
+  if (data.length === 1) {
+    let country = data[0];
+    let languages = Object.values(country.languages).join(', ');
+    countryInfoEl.innerHTML = `<h2><img height=36 src=${country.flags.svg} alt='Flag of ${country.name.official}'>${country.name.official}</h2>
+    <ul>
+      <li><b>Capital:</b> ${country.capital}</li>
+      <li><b>Population:</b> ${country.population}</li>
+      <li><b>Languages:</b> ${languages}</li>
+    </ul>`;
+  } else if (data.length > 1 && data.length < 11) {
+    let markup = ``;
+    for (let country of data) {
+      markup += `<li><img height=19px src=${country.flags.svg} alt='Flag of ${country.name.official}'>${country.name.official}</li>`;
+      listEl.innerHTML = markup;
+    }
+  } else {
+    Notify.info('Too many matches found. Please enter a more spcific name.');
+  }
+}
+
+inputEl.addEventListener(
+  'input',
+  debounce(event => {
+    countryInfoEl.innerHTML = '';
+    listEl.innerHTML = '';
+    fetchCountries(event.target.value.trim())
+      .then(data => {
+        renderCountries(data);
+      })
+      .catch(error => {
+        if (event.target.value === '') {
+          return;
+        } else {
+          Notify.failure('Oops, there is no country with that name');
+        }
+      });
+  }, DEBOUNCE_DELAY)
+);
